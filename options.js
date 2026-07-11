925a6b62e6e38a3cd75d71b358c1485b9e73075b
CoderFish1 <shreypandey22005@gmail.com>
2026-07-12 02:46:23 +0530

    options.js

---
+++ options.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    chrome.storage.sync.get(["geminiApiKey"], ({ geminiApiKey }) => {
+        if(geminiApiKey){
+            document.getElementById("api-key").value = geminiApiKey
+        }
+    });
+
+    document.getElementById("save-btn").addEventListener("click",()=>{
+        const apiKey = document.getElementById("api-key").value.trim();
+
+        if(!apikey) return;
+
+        chrome.storage.sync.set({geminiApiKey: apiKey},()=>{
+            document.getElementById("success").style.display = 'block'
+            setTimeout(() => {
+                windows.close()
+            }, 1000);
+        })
+    })
+})
